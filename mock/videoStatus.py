508fc98ebddc7483a8bf832837c71a9603f3ef26
link5209 <195140532@qq.com>
2017-03-17 05:51:44 -0400

    add multiprocessing for mock data

--- mock/videoStatus.py
+++ mock/videoStatus.py
@@ -1,11 +1,77 @@
 # from pymongo import MongoClient
 import psycopg2
+import psycopg2.extras
 import os
 import re
+import time
+import multiprocessing
 
-conn = psycopg2.connect("host=10.8.2.42 password=198325 dbname=onion user=postgres")
-pg = conn.cursor()
+dbUser = 'master'
+host = 'onions-test0314.cij1ctkoj9l3.rds.cn-north-1.amazonaws.com.cn'
 
-pg.execute('select * from video limit 2;')
-for row in pg:
-    print row
+# dbUser = 'postgres'
+# host = '10.8.2.42'
+
+conn = psycopg2.connect("host={} password=Yangcong345 dbname=onion user={}".format(host, dbUser))
+dictCursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# video map
+dictCursor.execute('select * from video;')
+videos = dictCursor.fetchall()
+dictCursor.close()
+conn.close()
+
+print 'video count: ', len(videos)
+
+# mock data
+def mock(limit, offset):
+    # conn
+    conn = psycopg2.connect("host={} password=Yangcong345 dbname=onion user={}".format(host, dbUser))
+    dictCursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # user
+    dictCursor.execute('select * from "user" LIMIT {} OFFSET {};'.format(limit, offset))
+
+    print 'limit, offset ', limit, offset
+
+    for row in dictCursor:
+        userId = row['id']
+
+        with conn.cursor() as videoStatusCur:
+            print 'conn..'
+
+            for video in videos:
+                videoId = video['id']
+                sql = """
+                    INSERT INTO "videoStatus" ("userId","videoId","finishTime",state) \
+                    VALUES (%s, %s, %s, %s) """
+
+                print 'pre insert'
+                videoStatusCur.execute(sql, (userId, videoId, None, 'unfinished'))
+                print 'insert'
+
+            conn.commit()
+            print 'commit'
+
+    dictCursor.close()
+    conn.close()
+
+
+# mock(1, 0)
+
+# multiprocessing
+beginTime = time.time()
+CPU_COUNT = 2
+LIMIT = 1
+
+pool = multiprocessing.Pool(processes=CPU_COUNT)
+for i in range(CPU_COUNT+1):
+    offset = i*LIMIT
+    pool.apply_async(mock, (LIMIT, offset, ))
+
+pool.close()
+pool.join()
+
+# all finished
+endTime = time.time()
+print 'all take time: ', endTime - beginTime
