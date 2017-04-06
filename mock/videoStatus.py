b70630a30fc8c8320d884dff33d67b609e34dcce
link5209 <195140532@qq.com>
2017-04-06 10:31:33 +0800

    add: try

--- mock/videoStatus.py
+++ mock/videoStatus.py
@@ -15,8 +15,12 @@ host = 'onions-test0314.cij1ctkoj9l3.rds.cn-north-1.amazonaws.com.cn'
 conn = psycopg2.connect("host={} password=Yangcong345 dbname=onion user={}".format(host, dbUser))
 dictCursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-# video map
-dictCursor.execute('select * from video;')
+# video map, exclude 初中物理
+dictCursor.execute("""
+    select count(*) from video v
+        where not exists
+        (select * from video v1 where v.subject = 'physics' and v.stage = 'middle');
+    """)
 videos = dictCursor.fetchall()
 dictCursor.close()
 conn.close()
@@ -40,28 +44,33 @@ def mock(limit, offset):
         with conn.cursor() as videoStatusCur:
 
             # TODO: 需要把用户完成视频的记录打散，更加随机
-            for video in videos:
-                videoId = video['id']
+            try:
+                for video in videos:
+                    videoId = video['id']
 
-                # TODO: 需要随机完成状态
-                sql = """
-                    INSERT INTO "videoStatus" ("userId","videoId","finishTime",state) \
-                    VALUES (%s, %s, %s, %s) """
+                    # TODO: 需要随机完成状态
+                    sql = """
+                        INSERT INTO "videoStatus" ("userId","videoId","finishTime",state) \
+                        VALUES (%s, %s, %s, %s) """
 
-                videoStatusCur.execute(sql, (userId, videoId, None, 'unfinished'))
+                    videoStatusCur.execute(sql, (userId, videoId, None, 'unfinished'))
 
-            conn.commit()
+                conn.commit()
+            except e:
+                raise e
 
     dictCursor.close()
     conn.close()
 
+    print 'finish: offset ', offset
+
 
 # mock(1, 0)
 
-# multiprocessing
+# multiprocessing, user count: [1, 2000]
 beginTime = time.time()
-CPU_COUNT = 2
-LIMIT = 1
+CPU_COUNT = 20
+LIMIT = 100
 
 pool = multiprocessing.Pool(processes=CPU_COUNT)
 for i in range(CPU_COUNT+1):
