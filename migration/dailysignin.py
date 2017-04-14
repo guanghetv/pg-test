6a7c75a31c8e682c0515de1f207aa6753e66e361
link5209 <195140532@qq.com>
2017-04-14 12:24:59 +0800

    add read line

--- migration/dailysignin.py
+++ migration/dailysignin.py
@@ -14,7 +14,7 @@ parentDir = os.path.dirname(currentDir)
 # dailysignins
 if (os.path.isfile(parentDir +'/data/dailysignins.csv') == False):
     os.system("""
-        ssh -n master@10.8.8.8 "mongoexport -h 10.8.8.8 -d onion40-0413 -c dailysignins --fields userId,level,nickname,channel,school,customSchool,aim,name,type,from,role,email,phone,coins,points,scores,region,nation,gender,province,semester,publisher,registTime,weekScores,activateDate,verifiedByPhone,vipExpirationTime,qqOpenId,createTime,learningTime,clientType,clientVersion,deviceId,userAgent,signInDate,year,month,day,hour,weekday,week --type=csv -o data/dailysignins.csv"
+        ssh -n master@10.8.8.8 "mongoexport -h 10.8.8.8 -d onion40-0413 -c dailysignins --fields userId,level,nickname,channel,school,customSchool,aim,name,type,from,role,email,phone,coins,points,scores,region,nation,gender,province,semester,publisher,registTime,weekScores,activateDate,verifiedByPhone,vipExpirationTime,qqOpenId,createTime,learningTime,clientType,clientVersion,deviceId,userAgent,signInDate,year,month,day,hour,weekday,week --type=csv -o data/dailysignins.csv" with (delimiter '|')
     """)
 
     print 'mongoexport finished'
@@ -26,32 +26,49 @@ if (os.path.isfile(parentDir +'/data/dailysignins.csv') == False):
 
     print 'scp finished'
 
-    # replace
-    filename = "data/dailysignins.csv"
-    with open(filename, 'r') as file:
-        result = re.sub(r'ObjectId\((.*?)\)', '\\1', file.read())
-    with open(filename, 'w') as f:
-        f.write(result)
+    # wirte file
+    wFile = open('data/dailysignins_new.csv', 'a')
 
-    print 'remove ObjectId tag finished'
+    # replace
+    filename = "data/dailysignins_t.csv"
+    with open(filename, 'r') as file:
+        for line in file:
+            #convert objectId
+            result = re.sub(r'ObjectId\((.*?)\)', '\\1', line)
+
+            # convert json
+            result = re.sub(r'"{""practice"":(.*?),""video"":(.*?)}"', '{"practice": \\1,"video":\\2}', result)
+        
+            # append into file
+            wFile.write(result)
+
+    wFile.close()
+
+    print 'convert format finished'
+
+
 
 
 # host = '10.8.2.42'
 # dbUser = 'postgres'
 
-# dbUser = 'master'
-# host = 'onion-t02.cij1ctkoj9l3.rds.cn-north-1.amazonaws.com.cn'
+dbUser = 'master'
+host = 'onion-t02.cij1ctkoj9l3.rds.cn-north-1.amazonaws.com.cn'
 
-# cmd = """
-#     PGPASSWORD=Yangcong345 \
-#     psql -h {} -p 5432 \
-#     -U {} -d onion \
-#     -c "\copy \\"user\\"(name,target,\\"customSchool\\",nickname,password, \
-#     channel,coins,points,type,gender,email,phone,\\"registTime\\",\\"from\\",role,salt,_id) \
-#     from '{}/data/dailysignins.csv' \
-#     delimiter as ',' csv header"
-# """.format(host, dbUser, parentDir)
+cmd = """
+    PGPASSWORD=Yangcong345 \
+    psql -h {} -p 5432 \
+    -U {} -d onion \
+    -c "\copy \\"dailySignin\\"(\\"userId\\",level,nickname,channel,school,\\"customSchool\\",
+        aim,name,type,\\"from\\",role,email,phone,coins,points,scores,region,nation,gender,province,
+        semester,publisher,\\"registTime\\",\\"weekScores\\",\\"activateDate\\",\\"verifiedByPhone\\",
+        \\"vipExpirationTime\\",\\"qqOpenId\\",\\"createTime\\",\\"learningTime\\",\\"clientType\\",
+        \\"clientVersion\\",\\"deviceId\\",\\"userAgent\\",\\"signInDate\\",
+        year,month,day,hour,\\"weekday\\",week)
+    from '{}/data/dailysignins_new.csv'
+    delimiter as '|' csv header"
+""".format(host, dbUser, parentDir)
 
-# print cmd
+print cmd
 
-# os.system(cmd)
+os.system(cmd)
