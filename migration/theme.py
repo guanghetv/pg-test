bdc22f3bf8d845bfb4acfab0d3fd3b96d457ed46
link5209 <jacklink5209@gmail.com>
2017-06-20 15:09:38 +0800

    modify _coverImages

--- migration/theme.py
+++ migration/theme.py
@@ -2,7 +2,7 @@
 
 from pymongo import MongoClient
 import psycopg2
-
+import psycopg2.extras
 
 # pymongo
 mongo = MongoClient(host='10.8.8.8', port=27017, connect=True)
@@ -25,55 +25,35 @@ conn = psycopg2.connect("""
 #     dbname=onion
 #     user=onions""")
 
-with conn.cursor() as cur:
+with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
     for chapter in chapters.find({'status': 'published'}):
         themeIds = chapter['themes']
 
         # 通过发布的章节找主题，避免无效的主题数据
         for theme in themes.find({'_id': {'$in': themeIds}, 'type': {'$ne': 'exam'}}):
-            # print theme
-
-            sql = """
-                INSERT INTO theme (
-                    "chapterId",
-                    "name",
-                    "pay",
-                    "state",
-                    "icons",
-                    "relatedThemeId",
-                    "hasPainPoint",
-                    "hasKeyPoint",
-                    "coverImage",
-                    "description",
-                    _id)
-                VALUES (
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    ARRAY[%s, %s]::theme_icon[],
-                    %s,
-                    %s,
-                    %s,
-                    ARRAY[%s],
-                    %s,
-                    %s
-                )"""
+            sql = None
 
             try:
+                # chapterId
+                pgChapter = cur.execute('select * from chapter where _id = %s;',
+                    (str(chapter['_id']),))
+
+                pgChapter = cur.fetchone()
+
+                # print pgChapter['id']
+                # exit()
+
                 # tranform
                 if ('desc' in theme and 'images' in theme['desc']):
-                    coverImage = theme['desc']['images']
-                    if not coverImage:
-                        coverImage = None
+                    coverImages = theme['desc']['images']
+                    if not coverImages:
+                        coverImages = None
                 else:
-                    coverImage = None
+                    coverImages = None
 
-                # if (coverImage is not None):
-                    # for image in coverImage:
-                    #     'ARRAY[]'.format()
-
-                # print 'coverImage: ', coverImage
+                _coverImages = None
+                if (coverImages is not None):
+                    _coverImages = "{"+ coverImages[0] +"}"
 
                 # desc.text
                 if ('desc' in theme and 'text' in theme['desc']):
@@ -103,19 +83,49 @@ with conn.cursor() as cur:
                 # exit()
 
                 # INSERT
-                sql = cur.mogrify(sql, (
-                    '87281f30-51a3-11e7-8034-833af1d88564', # 稍后需要更新
-                    theme['name'],
-                    theme['pay'] if ('pay' in theme) else False,
-                    'published', # 暂时都填充已发布
-                    i1, # theme_icon
-                    i2, # theme_icon
-                    None, # 稍后更新 str(theme['related']) if ('related' in theme) else None,
-                    theme['hasPainPoint'] if ('hasPainPoint' in theme) else False,
-                    theme['hasKeyPoint'] if ('hasKeyPoint' in theme) else False,
-                    'xxx',
-                    text,
-                    str(theme['_id'])))
+                sql = cur.mogrify("""
+                    INSERT INTO theme (
+                        "chapterId",
+                        "name",
+                        "pay",
+                        "state",
+                        "icons",
+                        "relatedThemeId",
+                        "related",
+                        "hasPainPoint",
+                        "hasKeyPoint",
+                        "coverImages",
+                        "description",
+                        _id)
+                    VALUES (
+                        %s,
+                        %s,
+                        %s,
+                        %s,
+                        ARRAY[%s, %s]::theme_icon[],
+                        %s,
+                        %s,
+                        %s,
+                        %s,
+                        %s,
+                        %s,
+                        %s
+                    )
+                    """,
+                    (
+                        pgChapter['id'],
+                        theme['name'],
+                        theme['pay'] if ('pay' in theme) else False,
+                        'published', # 暂时都填充已发布
+                        i1,
+                        i2,
+                        None, # 稍后更新 relatedThemeId
+                        str(theme['related']) if ('related' in theme) else None,
+                        theme['hasPainPoint'] if ('hasPainPoint' in theme) else False,
+                        theme['hasKeyPoint'] if ('hasKeyPoint' in theme) else False,
+                        _coverImages,
+                        text,
+                        str(theme['_id'])))
 
                 # print sql
                 # exit()
@@ -124,11 +134,13 @@ with conn.cursor() as cur:
                 conn.commit()
             except Exception as e:
                 print sql
+                print chapter
                 print theme
 
                 print 'error: ', e
                 conn.rollback()
 
-                raise e
+                exit()
+                # raise e
 
 conn.close()
